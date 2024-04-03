858bf9098a0f5ebabea5e58fcf3325c09dcc2bb6
Joao-M10 <joaomsl@id.uff.br>
2024-04-03 19:02:05 -0300

    tab bar perfil

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
-import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons, Octicons } from '@expo/vector-icons'
 
 const Layout = () => {
   return (
@@ -9,6 +9,14 @@ const Layout = () => {
       <Tabs.Screen name='index' options={{
         tabBarLabel: 'Buscar',
         tabBarIcon: ({color,size}) => <MaterialIcons name="search" size={size} color={color}></MaterialIcons>,
+        tabBarLabelStyle: ({fontSize:12}),
+        tabBarIconStyle: { marginBottom: -5 }
+      }
+      }
+      ></Tabs.Screen>
+      <Tabs.Screen name='perfil' options={{
+        tabBarLabel: 'Perfil',
+        tabBarIcon: ({color,size}) => <Octicons name="person" size={size} color={color}></Octicons>,
         tabBarLabelStyle: ({fontSize:12}),
         tabBarIconStyle: { marginBottom: -5 }
       }
