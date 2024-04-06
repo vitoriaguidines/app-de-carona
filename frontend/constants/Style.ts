07456446185dc6a11eb4672063eef73b669b52ce
Joao-M10 <joaomsl@id.uff.br>
2024-04-05 22:45:13 -0300

    update flex containers

--- frontend/constants/Style.ts
+++ frontend/constants/Style.ts
@@ -1,4 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 
 export const defaultStyles = StyleSheet.create({
   container: {
@@ -7,22 +11,21 @@ export const defaultStyles = StyleSheet.create({
     alignItems: 'center',
   },
   uff: {
-    height: '72%',
-    width: '100%',
+    height: windowHeight * 0.5, // 72% da altura da tela
+    width: windowWidth, // largura total da tela
     justifyContent: 'center',
     alignItems: 'center',
-    top: '2%'
+    bottom: windowHeight * 0.07
   },
   rectangle: {
-    width: '80%',  
-    height: '35%', 
+    width: windowWidth * 0.8, // 80% da largura da tela
+    height: windowHeight * 0.35, // 35% da altura da tela
     backgroundColor: '#262A2B', 
     borderRadius: 23,
-    bottom: '25%'
+    bottom: windowHeight * 0.17 // 25% da altura da tela
   },
   logo: {
-    width: '40%',
-    height: '35%',
-    bottom: '11%'
+    width: windowWidth * 0.4, // 40% da largura da tela
+    height: windowHeight * 0.23, // 35% da altura da tela
   },
 });
