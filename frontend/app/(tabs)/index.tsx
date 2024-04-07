1c3b165b67c6c99dafbfb453775a3b26c8923463
Joao-M10 <joaomsl@id.uff.br>
2024-04-06 22:32:20 -0300

    navigate pedir carona

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, ImageBackground, Image, Text } from 'react-native';
+import { View, ImageBackground, Image, Text, TouchableOpacity } from 'react-native';
 import { defaultStyles } from '@/constants/Style'; 
 import uffBackground from '@/assets/images/uff.png';
 import uffLogo from '@/assets/images/logouff.png';
-import { Link } from 'expo-router';
+import { Link, useNavigation } from 'expo-router';
 
 const Stack = createNativeStackNavigator();
 
-export  function BuscarScreen() {
+export function BuscarScreen() {
+  const navigation = useNavigation();
+
+  const handleNavigateToMap = () => {
+    navigation.navigate('(models)/mapa');
+  };
+
   return (
     <View style={[defaultStyles.container, { backgroundColor: '#131514' }]}>
       <ImageBackground source={uffBackground} style={defaultStyles.uff} blurRadius={8}>
         <Image source={uffLogo} style={defaultStyles.logo} />
       </ImageBackground>
-        <View style={defaultStyles.rectangle}>
-          <View style={defaultStyles.endereco}/>
-            <View style={defaultStyles.blueSection}>
-              <Text style={[{fontSize: 24,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Procurar</Text>
-              <Link href={'/(models)/mapa'}/>
-            </View>
-        </View>
+      <View style={defaultStyles.rectangle}>
+        <View style={defaultStyles.endereco}/>
+        <TouchableOpacity style={defaultStyles.blueSection} onPress={(handleNavigateToMap)}>
+          <Text style={[{fontSize: 24,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Procurar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
