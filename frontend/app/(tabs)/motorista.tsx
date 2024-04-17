cc2c72112b75283a63e1a07ba988de4d9121dcb8
Joao-M10 <joaomsl@id.uff.br>
2024-04-17 20:32:04 -0300

    update motorista

--- frontend/app/(tabs)/motorista.tsx
+++ frontend/app/(tabs)/motorista.tsx
@@ -18,15 +18,17 @@ const Motorista = () => {
   };
 
   return (
-    <View style={[defaultStyles.container, { backgroundColor: '#131514' }]}>
-      <View style={defaultStyles.container}> 
+    <View style={[defaultStyles.container, { backgroundColor: '#131514', justifyContent:'center'}]}>
+      <Text style={[{fontSize: 30,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>De onde você vai sair?</Text>
+      <View style={[defaultStyles.container, {flex:0, flexDirection:'row'}]}> 
         <TouchableOpacity style={defaultStyles.endereco} onPress={handleNavigateToMap}>
-          <Text style={[{fontSize: 24,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Origem</Text>
+          <Text style={[{fontSize: 15,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Insira o endereço completo</Text>
         </TouchableOpacity>
       </View>
-      <View style={[defaultStyles.container, {flexDirection:'row'}]}> 
-        <TouchableOpacity style={defaultStyles.endereco} onPress={handleNavigateToReserva}>
-          <Text style={[{fontSize: 24,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Destino</Text>
+      <Text style={[{fontSize: 30,color: '#fff', fontWeight: 'bold',textAlign: 'center', marginTop: 12.5}]}>Pra onde você vai?</Text>
+      <View style={[defaultStyles.container, {flexDirection:'row', flex: 0}]}> 
+        <TouchableOpacity style={defaultStyles.endereco} onPress={handleNavigateToMap}>
+          <Text style={[{fontSize: 15,color: '#fff', fontWeight: 'bold',textAlign: 'center'}]}>Insira o endereço completo</Text>
         </TouchableOpacity>
       </View>
     </View>
