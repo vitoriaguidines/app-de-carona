4e8f36f851b918c93c5ce26f1a7da4424c9e3f04
Joao-M10 <joaomsl@id.uff.br>
2024-04-18 19:13:52 -0300

    relogio tela motorista

--- frontend/app/(tabs)/motorista.tsx
+++ frontend/app/(tabs)/motorista.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Platform } from 'react-native';
 import { defaultStyles } from '@/constants/Style'; 
 import { useNavigation } from 'expo-router';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Feather } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 
 const Motorista = () => {
   const navigation = useNavigation();
+  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [isTimePickerVisible, setIsTimePickerVisible] = useState(Platform.OS === 'ios');
+
+  const handleTimeChange = (event, time) => {
+    setSelectedTime(time || selectedTime);
+  };
+
+  const showTimePicker = () => {
+    setIsTimePickerVisible(true);
+  };
+
+  const hideTimePicker = () => {
+    setIsTimePickerVisible(false);
+  };
 
   const handleNavigateToMap = () => {
     navigation.navigate('(models)/mapa');
@@ -26,7 +42,7 @@ const Motorista = () => {
         </TouchableOpacity>
       </View>
       <View style={defaultStyles.separator} />
-      <Text style={[{fontSize: 30,color: '#fff', fontWeight: 'bold',textAlign: 'left', marginTop: 12.5}]}>Pra onde você vai?</Text>
+      <Text style={[{fontSize: 30,color: '#fff', fontWeight: 'bold',textAlign: 'left'}]}>Pra onde você vai?</Text>
       <View style={[defaultStyles.container, {flexDirection:'row', flex: 0}]}> 
         <TouchableOpacity style={defaultStyles.motorista} onPress={handleNavigateToMap}>
           <Text style={[{fontSize: 15,color: '#626262',textAlign: 'center'}]}>Insira o endereço completo</Text>
@@ -34,11 +50,26 @@ const Motorista = () => {
       </View>
       <View style={defaultStyles.separator} />
       <Text style={[{fontSize: 30,color: '#fff', fontWeight: 'bold',textAlign: 'left'}]}>Horário de Partida</Text>
-      <View style={[defaultStyles.container, {flex:0, flexDirection:'row'}]}> 
-        <TouchableOpacity style={defaultStyles.motorista} onPress={handleNavigateToMap}>
-          <Text style={[{fontSize: 15,color: '#626262',textAlign: 'center'}]}>Horário</Text>
+      {/* Seletor de hora */}
+      {Platform.OS === 'android' && (
+        <TouchableOpacity onPress={showTimePicker} style={{flexDirection:'row', alignItems:'center'}}>
+                      <Feather name="clock" size={24} color="#0F62AC" />
+                      <Text style={{ fontSize: 20, color: '#fff' }}>{selectedTime.toLocaleTimeString()}</Text>
         </TouchableOpacity>
-      </View>
+      )}
+      {isTimePickerVisible && (
+          <DateTimePicker
+          value={selectedTime}
+          mode="time"
+          display={Platform.OS === 'ios' ? 'spinner' : 'clock'}
+          onChange={(event, time) => {
+            handleTimeChange(event, time);
+            if (Platform.OS === 'android') {
+              hideTimePicker();
+            }
+          }}
+        />
+      )}
     </View>
   );
 }
