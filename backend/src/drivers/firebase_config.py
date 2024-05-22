665d552c5f63d312a5402a3db9a9f53afb0336a6
Vitória Guidine <78456655+vitoriaguidines@users.noreply.github.com>
2024-05-21 23:32:35 -0300

    json add

--- backend/src/drivers/firebase_config.py
+++ backend/src/drivers/firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 
 # Inicialize o app Firebase com as credenciais do arquivo JSON e a URL do Realtime Database
 def initialize_firebase_app():
-    cred = credentials.Certificate("src/drivers/app-de-carona-firebase-adminsdk-p4bg0-3f429eaea0.json")
+    cred = credentials.Certificate("src/keys/app-de-carona-firebase-adminsdk-p4bg0-784866d24b.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://app-de-carona-default-rtdb.firebaseio.com/'
     })
