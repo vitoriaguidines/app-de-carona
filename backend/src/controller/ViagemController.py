a3c3f34f0368bbd6fe589b138f25f7d47803ea08
marws <112357654+tomariacecilia@users.noreply.github.com>
2024-06-08 22:59:21 -0300

    adicionar import datetime

--- backend/src/controller/ViagemController.py
+++ backend/src/controller/ViagemController.py
@@ -4,6 +4,7 @@ from firebase_admin import db
 from src.controller.GooglemapsController import MapsController
 from src.drivers.firebase_config import initialize_firebase_app
 from src.views.http_types.http_response import HttpResponse
+from datetime import datetime
 
 class ViagemController:
         
